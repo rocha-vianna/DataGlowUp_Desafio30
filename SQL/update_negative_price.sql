501b79ae12a83ac026169abeac02c9f51ee154ff
João Rocha Vianna <118622964+rocha-vianna@users.noreply.github.com>
2024-02-20 21:25:20 -0300

    Add files via upload

---
+++ SQL/update_negative_price.sql
@@ -0,0 +1,104 @@
+UPDATE october
+SET price = 15.87
+WHERE product_id = '5670257';
+
+UPDATE november
+SET price = 15.87
+WHERE product_id = '5670257';
+
+UPDATE december
+SET price = 15.87
+WHERE product_id = '5670257';
+
+UPDATE january
+SET price = 15.87
+WHERE product_id = '5670257';
+
+UPDATE february
+SET price = 15.87
+WHERE product_id = '5670257';
+----------------------------------------------
+
+UPDATE october
+SET price = 7.94
+WHERE product_id = '5716855';
+
+UPDATE november
+SET price = 7.94
+WHERE product_id = '5716855';
+
+UPDATE december
+SET price = 7.94
+WHERE product_id = '5716855';
+
+UPDATE january
+SET price = 7.94
+WHERE product_id = '5716855';
+
+UPDATE february
+SET price = 7.94
+WHERE product_id = '5716855';
+----------------------------------------------
+
+UPDATE october
+SET price = 23.81
+WHERE product_id = '5716857';
+
+UPDATE november
+SET price = 23.81
+WHERE product_id = '5716857';
+
+UPDATE december
+SET price = 23.81
+WHERE product_id = '5716857';
+
+UPDATE january
+SET price = 23.81
+WHERE product_id = '5716857';
+
+UPDATE february
+SET price = 23.81
+WHERE product_id = '5716857';
+----------------------------------------------
+
+UPDATE october
+SET price = 47.62
+WHERE product_id = '5716859';
+
+UPDATE november
+SET price = 47.62
+WHERE product_id = '5716859';
+
+UPDATE december
+SET price = 47.62
+WHERE product_id = '5716859';
+
+UPDATE january
+SET price = 47.62
+WHERE product_id = '5716859';
+
+UPDATE february
+SET price = 47.62
+WHERE product_id = '5716859';
+----------------------------------------------
+
+UPDATE october
+SET price = 79.37
+WHERE product_id = '5716861';
+
+UPDATE november
+SET price = 79.37
+WHERE product_id = '5716861';
+
+UPDATE december
+SET price = 79.37
+WHERE product_id = '5716861';
+
+UPDATE january
+SET price = 79.37
+WHERE product_id = '5716861';
+
+UPDATE february
+SET price = 79.37
+WHERE product_id = '5716861';
+----------------------------------------------
